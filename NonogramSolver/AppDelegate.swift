a41304f565743d7628ad8b414fd01b3efa1352aa
fnc12 <fnc12345@gmail.com>
2018-01-05 21:01:41 +0300

    Initial Commit

---
+++ NonogramSolver/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  NonogramSolver
+//
+//  Created by John Zakharov on 05.01.2018.
+//  Copyright © 2018 John Zakharov. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ NonogramSolver/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  NonogramSolver
+//
+//  Created by John Zakharov on 05.01.2018.
+//  Copyright © 2018 John Zakharov. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
